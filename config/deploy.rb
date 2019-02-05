43059bae8017fc4327b6cf41258a6711a6d47846
Diskominfo <diskominfo@MacBook-Pro.local>
2019-02-06 02:19:06 +0800

    new

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-set :application, "bradenscale"
-set :repo_url, "https://github.com/bilalteknologi/bradenscale.git"
+set :application, "dysphagiaassesment"
+set :repo_url, "https://github.com/muhrizkiakbar/dysphagiaassesment.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/deploy/bradenscale"
+set :deploy_to, "/home/deploy/dysphagiaassesment"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
