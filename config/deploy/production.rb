cf150c5d43e115457b4ca06249611e77554ffca2
muhrizkiakbar <rizkiakbarzein@gmail.com>
2019-08-14 02:47:03 +0800

    bew

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server '213.190.4.55', user: 'deploy', roles: %w{app db web}
+server '194.31.53.96', user: 'deploy', roles: %w{app db web}
 
 
 # role-based syntax
